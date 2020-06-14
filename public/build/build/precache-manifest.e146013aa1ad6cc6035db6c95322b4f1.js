a2e950774ebf142b6a2f6a283e7a30cc6f5a0173
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-06-14 22:07:23 +0800

    Build client files and moved to relevant directory for deployment

---
+++ public/build/build/precache-manifest.e146013aa1ad6cc6035db6c95322b4f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb5077d33bd70d6c8a11050e692c3cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af717d7997a13f6983fb",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a6f788a7489743d087db",
+    "url": "/static/js/2.251fd67b.chunk.js"
+  },
+  {
+    "revision": "013708584ae97a744ec506df2bba30b2",
+    "url": "/static/js/2.251fd67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af717d7997a13f6983fb",
+    "url": "/static/js/main.3c86fcd6.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "c75119a23bdcf40e7c36a993f0ea7657",
+    "url": "/static/media/logo.c75119a2.jpg"
+  }
+]);
