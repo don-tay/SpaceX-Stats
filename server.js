36fe058fd14c9dbdce7d2b17e0c5f757655785b9
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-06-15 01:05:12 +0800

    Rectify port binding bug

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () =>
+app.listen(PORT, () =>
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
